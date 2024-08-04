3884cc1f8ade190dbe0d4fcac130866a3a6bacec
Akshalin Jefita R J <136790392+Akshalin@users.noreply.github.com>
2024-08-04 23:27:45 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,65 @@
+function red(){
+    document.getElementsByClassName("option")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function red1(){
+    document.getElementsByClassName("option1")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function red2(){
+    document.getElementsByClassName("option2")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function green(){
+    document.getElementsByClassName("option3")[0].style.backgroundColor="green";
+    document.getElementsByClassName("green")[0].style="background-color:green;border-color:green;";
+}
+function green1(){
+    document.getElementsByClassName("option4")[0].style.backgroundColor="green";
+    document.getElementsByClassName("green1")[0].style="background-color:green;border-color:green;";
+}
+function red3(){
+    document.getElementsByClassName("option5")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function red4(){
+    document.getElementsByClassName("option6")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function red5(){
+    document.getElementsByClassName("option7")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function red6(){
+    document.getElementsByClassName("option8")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function green2(){
+    document.getElementsByClassName("option9")[0].style.backgroundColor="green";
+    document.getElementsByClassName("green2")[0].style="background-color:green;border-color:green;";
+}
+function red7(){
+    document.getElementsByClassName("option10")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function red8(){
+    document.getElementsByClassName("option11")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function green3(){
+    document.getElementsByClassName("option12")[0].style.backgroundColor="green";
+    document.getElementsByClassName("green3")[0].style="background-color:green;border-color:green;";
+}
+function red9(){
+    document.getElementsByClassName("option13")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function red10(){
+    document.getElementsByClassName("option14")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function red11(){
+    document.getElementsByClassName("option15")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function green4(){
+    document.getElementsByClassName("option16")[0].style.backgroundColor="green";
+    document.getElementsByClassName("green4")[0].style="background-color:green;border-color:green;";
+}
+function red12(){
+    document.getElementsByClassName("option17")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function red13(){
+    document.getElementsByClassName("option18")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
+function red14(){
+    document.getElementsByClassName("option19")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
+}
