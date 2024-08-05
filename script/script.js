52001b1efe949f10a065541b58091e64093c6db9
Akshalin Jefita R J <136790392+Akshalin@users.noreply.github.com>
2024-08-05 17:31:02 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -10,10 +10,16 @@ function red2(){
 function green(){
     document.getElementsByClassName("option3")[0].style.backgroundColor="green";
     document.getElementsByClassName("green")[0].style="background-color:green;border-color:green;";
+    document.getElementsByClassName("option2")[0].style.backgroundColor="white";
+    document.getElementsByClassName("option")[0].style.backgroundColor="white";
+    document.getElementsByClassName("option1")[0].style.backgroundColor="white";
 }
 function green1(){
     document.getElementsByClassName("option4")[0].style.backgroundColor="green";
     document.getElementsByClassName("green1")[0].style="background-color:green;border-color:green;";
+    document.getElementsByClassName("option5")[0].style.backgroundColor="white";
+    document.getElementsByClassName("option6")[0].style.backgroundColor="white";
+    document.getElementsByClassName("option7")[0].style.backgroundColor="white";
 }
 function red3(){
     document.getElementsByClassName("option5")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
@@ -30,6 +36,9 @@ function red6(){
 function green2(){
     document.getElementsByClassName("option9")[0].style.backgroundColor="green";
     document.getElementsByClassName("green2")[0].style="background-color:green;border-color:green;";
+    document.getElementsByClassName("option8")[0].style.backgroundColor="white";
+    document.getElementsByClassName("option10")[0].style.backgroundColor="white";
+    document.getElementsByClassName("option11")[0].style.backgroundColor="white";
 }
 function red7(){
     document.getElementsByClassName("option10")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
@@ -40,6 +49,9 @@ function red8(){
 function green3(){
     document.getElementsByClassName("option12")[0].style.backgroundColor="green";
     document.getElementsByClassName("green3")[0].style="background-color:green;border-color:green;";
+    document.getElementsByClassName("option13")[0].style.backgroundColor="white";
+    document.getElementsByClassName("option14")[0].style.backgroundColor="white";
+    document.getElementsByClassName("option15")[0].style.backgroundColor="white";
 }
 function red9(){
     document.getElementsByClassName("option13")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
@@ -53,6 +65,9 @@ function red11(){
 function green4(){
     document.getElementsByClassName("option16")[0].style.backgroundColor="green";
     document.getElementsByClassName("green4")[0].style="background-color:green;border-color:green;";
+    document.getElementsByClassName("option17")[0].style.backgroundColor="white";
+    document.getElementsByClassName("option18")[0].style.backgroundColor="white";
+    document.getElementsByClassName("option19")[0].style.backgroundColor="white";
 }
 function red12(){
     document.getElementsByClassName("option17")[0].style.backgroundColor="rgba(255, 0, 0, 0.753)";
